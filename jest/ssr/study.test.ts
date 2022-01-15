4bb0cc7d470da44fcf2d787448a13c656256be2b
Mikhail Kudashev <komandarmy@rambler.ru>
2022-01-15 20:54:55 +0300

    add new test 4.1 4.2
    test errors fixed

--- jest/ssr/study.test.ts
+++ jest/ssr/study.test.ts
@@ -10,14 +10,19 @@ test('Meeting page has meta', async () => {
 
   // <title>London is a capital of Great Britain, Geography, December 2nd, 2019 10:00 — 11:00, Beginner, Kaya Edwards — Study | OnClass</title>
   const title = $('title').text();
+  
   // TODO проверить что там текст "London is a capital of Great Britain"
+  expect(title).toContain("London is a capital of Great Britain");
 
   // <meta name="description" content="Lorem ipsum dolor sit amet<...>">
   const description = $('meta[name="description"]').attr('content').toString().trim();
   // TODO Проверить что там есть текст Lorem ipsum dolor sit amet
+  expect(description).toContain("Lorem ipsum dolor sit amet");
 
   // TODO const h1 = $('app-meeting-card h1').toString().trim();
+  const h1 = $('app-meeting-card h1').toString().trim();
   // TODO проверить что там текст "London is a capital of Great Britain"
+  expect(h1).toContain("London is a capital of Great Britain");
 
   // TODO const img = $('app-meeting-card img.cover-img').attr(???).toString().trim();
   // TODO проверить что там URL картинки
