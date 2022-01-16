778ae9b9dc4caa86c09c1880befefde2d1fa03fe
Mikhail Kudashev <komandarmy@rambler.ru>
2022-01-16 15:07:42 +0300

    add test 4.3

--- jest/ssr/study.test.ts
+++ jest/ssr/study.test.ts
@@ -1,6 +1,8 @@
 import * as cheerio from 'cheerio';
 import { get } from '../request';
 import { environment } from '../../src/environments/environment';
+import { URL } from 'url';
+import { url } from 'inspector';
 
 test('Meeting page has meta', async () => {
   const response = await get(`${environment.portal_url}/study/geography/group-lessons/London-is-a-capital-of-Great-Britain-r0fa4z0f`);
@@ -17,7 +19,7 @@ test('Meeting page has meta', async () => {
   // <meta name="description" content="Lorem ipsum dolor sit amet<...>">
   const description = $('meta[name="description"]').attr('content').toString().trim();
   // TODO Проверить что там есть текст Lorem ipsum dolor sit amet
-  expect(description).toContain("Lorem ipsum dolor sit amet");
+  expect(description).toContain('Lorem ipsum dolor sit amet');
 
   // TODO const h1 = $('app-meeting-card h1').toString().trim();
   const h1 = $('app-meeting-card h1').toString().trim();
@@ -25,6 +27,8 @@ test('Meeting page has meta', async () => {
   expect(h1).toContain("London is a capital of Great Britain");
 
   // TODO const img = $('app-meeting-card img.cover-img').attr(???).toString().trim();
+  const img = $('app-meeting-card img.cover-img').attr('src').toString().trim();
   // TODO проверить что там URL картинки
+  expect(img).toContain('.jpg');
+  
 });
-
